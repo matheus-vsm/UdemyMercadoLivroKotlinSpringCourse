a8d39ffcb6c079e135024a39412f75d44e65721a
matheus-vsm <scompinha@hotmail.com>
2025-12-01 13:54:52 -0300

    aula 5.35

--- src/main/kotlin/br/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/br/com/mercadolivro/controller/CustomerController.kt
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
@@ -21,7 +22,9 @@ class CustomerController {
     val customers = mutableListOf<Customer>()
 
     @GetMapping
-    fun getAll(): List<Customer> {
+    fun getAll(@RequestParam nome: String?): List<Customer> {
+        // http://localhost:8080/customers?nome=Math
+        nome?.let { return customers.filter { it.nome.contains(nome, true) } }
         return customers
     }
 
