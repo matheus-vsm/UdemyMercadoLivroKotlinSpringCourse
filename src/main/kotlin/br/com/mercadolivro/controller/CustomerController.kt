c6a6aa718e8fddb66daae08d5c87f26152d6a2d8
matheus-vsm <scompinha@hotmail.com>
2025-12-01 11:50:31 -0300

    aula 5.30

--- src/main/kotlin/br/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/br/com/mercadolivro/controller/CustomerController.kt
@@ -2,10 +2,12 @@ package br.com.mercadolivro.controller
 
 import br.com.mercadolivro.controller.request.PostCustomerRequest
 import br.com.mercadolivro.model.Customer
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -22,6 +24,7 @@ class CustomerController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody customer: PostCustomerRequest) {
         println(customer)
     }
