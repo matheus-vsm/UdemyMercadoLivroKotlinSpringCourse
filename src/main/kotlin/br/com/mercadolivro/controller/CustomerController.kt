2bf782cfd714bee2535d3c82808fc93dc5f29a5e
matheus-vsm <scompinha@hotmail.com>
2025-12-01 11:28:27 -0300

    aula 5.27

--- src/main/kotlin/br/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/br/com/mercadolivro/controller/CustomerController.kt
@@ -1,12 +1,21 @@
 package br.com.mercadolivro.controller
 
+import br.com.mercadolivro.model.Customer
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("customers")
 class CustomerController {
+
     @GetMapping
-    fun helloWorld(): String {
-        return "Hello World"
+    fun getCustomer(): Customer {
+        return Customer(
+            id = "1",
+            name = "John Doe",
+            email = "john@gmail.com")
     }
+
+
 }
